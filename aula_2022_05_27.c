bb6af0818eae674c449fd3b49ec75663b8d0dac1
Erikefb <106414223+Erikefb@users.noreply.github.com>
2022-05-27 22:06:26 -0300

    Add files via upload

---
+++ aula_2022_05_27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+printf("..........oi mundo..........\n");
+printf("%d \n", 16);
+printf("%05.2f \n", 7.14);
+printf("%05.2f \n", 27.2);
+printf("%s \n", "eu sou lindo");
+printf("............................\n");
+printf("nome:%s \nidade:%d \n", "Érik Feitosa Barbosa", 19);
+
+}
+
