00e87dd80a4c175e83fe257f075b57e4e4d4b265
Erikefb <106414223+Erikefb@users.noreply.github.com>
2022-06-02 19:51:01 -0300

    Update aula-2022-06-02.c

--- exercicios_de_sala/aula-2022-06-02.c
+++ exercicios_de_sala/aula-2022-06-02.c
@@ -1,1 +1,34 @@
+/*matrícula: 2021100625
+algorítmo: perguntar as entradas de segunda-feira, anotar as entradas de segunda-feira, o mesmo para as saídas e analogamente fazer isso com os outros dias da semana exceto sábado e domingo.
+*/
+#include <stdio.h>
+int main(){
 
+float segunda, segundas, terca, tercas, quarta, quartas, quinta, quintas, sexta, sextas, pagamento, soma;
+
+printf("Escreva suas entradas de segunda-feira:\n");
+scanf("%f", &segunda);
+printf("Escreva suas saídas de segunda-feira:\n");
+scanf("%f", &segundas);
+printf("Escreva suas entradas de terça-feira:\n");
+scanf("%f", &terca);
+printf("Escreva suas saídas de terça-feira:\n");
+scanf("%f", &tercas);
+printf("Escreva suas entradas de quarta-feira:\n");
+scanf("%f", &quarta);
+printf("Escreva suas saídas de quarta-feira:\n");
+scanf("%f", &quartas);
+printf("Escreva suas entradas de quinta-feira:\n");
+scanf("%f", &quinta);
+printf("Escreva suas saídas de quinta-feira:\n");
+scanf("%f", &quintas);
+printf("Escreva suas entradas de sexta-feira:\n");
+scanf("%f", &sexta);
+printf("Escreva suas saídas de sexta-feira:\n");
+scanf("%f", &sextas);
+
+
+
+
+
+}
