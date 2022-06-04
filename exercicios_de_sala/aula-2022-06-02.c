f576a19a2c04914401dc72a2443287f3f2a8fb5d
Erikefb <106414223+Erikefb@users.noreply.github.com>
2022-06-03 21:52:24 -0300

    Update aula-2022-06-02.c

--- exercicios_de_sala/aula-2022-06-02.c
+++ exercicios_de_sala/aula-2022-06-02.c
@@ -1,17 +1,43 @@
-/*matrícula: 2021100625
+/*Aluno: Érik Feitosa Barbosa.
+*matrícula: 2021100625
 *algorítmo: 
 *variáveis:
-*float: segunda, segundas, terca, tercas, quarta, quartas, quinta, quintas, sexta, sextas, despesas, recebidos, caixa;
+*float: segunda, segundas, caixas, terca, tercas, caixat, quarta, quartas, caixaq, quinta, quintas, caixaqi, sexta, sextas, caixasx;
 *
-*declarar as variaveis reais de entrada e despesas para cada dia.
+*início: pedir os valores de entrada da segunda-feira e ler o valor em "segunda"
+*pedir os valores de saída da segunda-feira e ler o valor em "segundas"
+*caixas = segunda - segundas
+*se caixas > 2000: saldo positivo
+*senão se caixas < -500: saldo negativo
 *
+*pedir os valores de entrada da terça-feira e ler o valor em "terca"
+*pedir os valores de saída da terça-feira e ler o valor em "tercas"
+*caixat = terca - tercas
+*se caixat > 2000: saldo positivo
+*senão se caixat < -500: saldo negativo
 *
+*pedir os valores de entrada da quarta-feira e ler o valor em "quarta"
+*pedir os valores de saída da quarta-feira e ler o valor em "quartas"
+*caixaq = quarta - quartas
+*se caixaq > 2000: saldo positivo
+*senão se caixaq < -500: saldo negativo
 *
+*pedir os valores de entrada da quinta-feira e ler o valor em "quinta"
+*pedir os valores de saída da quinta-feira e ler o valor em "quintas"
+*caixaqi = quinta - quintas
+*se caixaq > 2000: saldo positivo
+*senão se caixaq < -500: saldo negativo
+*
+*pedir os valores de entrada da sexta-feira e ler o valor em "sexta"
+*pedir os valores de saída da sexta-feira e ler o valor em "sextas"
+*caixasx = sexta - sextas
+*se caixaq > 2000: saldo positivo
+*senão se caixaq < -500: saldo negativo
 */
 #include <stdio.h>
 int main(){
 
-float segunda, segundas,caixas, terca, tercas,caixat, quarta, quartas, caixaq, quinta, quintas, caixaqi, sexta, sextas, caixasx;
+float segunda, segundas, caixas, terca, tercas, caixat, quarta, quartas, caixaq, quinta, quintas, caixaqi, sexta, sextas, caixasx;
 
 //segunda-feira
 printf("Escreva suas entradas de segunda-feira:\n");
@@ -19,17 +45,17 @@ scanf("%f", &segunda);
 printf("Escreva suas saídas de segunda-feira:\n");
 scanf("%f", &segundas);
 caixas=segunda-segundas;
-printf("Você tem um saldo de:\n", caixas);
+printf("Você tem um saldo de:%f\n", caixas);
 
 if (caixas > 2000){
-printf("O valor total recebido foi de:\n", segunda);
-printf("O total de gastos é igual a:\n", segundas);
-printf("Você possui um saldo de:\n", caixas);
+printf("O valor total recebido foi de:%f\n", segunda);
+printf("O total de gastos é igual a:%f\n", segundas);
+printf("Você possui um saldo positivo de:%f\n", caixas);
 }
 else if(caixas < -500){
-printf("O valor total recebido foi de:\n", segunda);
-printf("O total de gastos é igual a:\n", segundas);
-printf("Você possui um saldo de:\n", caixas);
+printf("O valor total recebido foi de:%f\n", segunda);
+printf("O total de gastos é igual a:%f\n", segundas);
+printf("Você possui um saldo negativo de:%f\n", caixas);
 }
 
 //terça-feira
@@ -38,17 +64,17 @@ scanf("%f", &terca);
 printf("Escreva suas saídas de terça-feira:\n");
 scanf("%f", &tercas);
 caixat=terca-tercas;
-printf("Você tem um saldo de:\n", caixat);
+printf("Você tem um saldo de:%f\n", caixat);
 
 if (caixat > 2000){
-printf("O valor total recebido foi de:\n", terca);
-printf("O total de gastos é igual a:\n", tercas);
-printf("Você possui um saldo de:\n", caixat);
+printf("O valor total recebido foi de:%f\n", terca);
+printf("O total de gastos é igual a:%f\n", tercas);
+printf("Você possui um saldo positivo de:%f\n", caixat);
 }
 else if(caixat < -500){
-printf("O valor total recebido foi de:\n", terca);
-printf("O total de gastos é igual a:\n", tercas);
-printf("Você possui um saldo de:\n", caixat);
+printf("O valor total recebido foi de:%f\n", terca);
+printf("O total de gastos é igual a:%f\n", tercas);
+printf("Você possui um saldo negativo de:%f\n", caixat);
 }
 
 //quarta-feira
@@ -57,17 +83,17 @@ scanf("%f", &quarta);
 printf("Escreva suas saídas de quarta-feira:\n");
 scanf("%f", &quartas);
 caixaq=quarta-quartas;
-printf("Você tem um saldo de:\n", caixaq);
+printf("Você tem um saldo de:%f\n", caixaq);
 
 if (caixaq > 2000){
-printf("O valor total recebido foi de:\n", quarta);
-printf("O total de gastos é igual a:\n", quartas);
-printf("Você possui um saldo de:\n", caixaq);
+printf("O valor total recebido foi de:%f\n", quarta);
+printf("O total de gastos é igual a:%f\n", quartas);
+printf("Você possui um saldo positivo de:%f\n", caixaq);
 }
 else if(caixaq < -500){
-printf("O valor total recebido foi de:\n", quarta);
-printf("O total de gastos é igual a:\n", quartas);
-printf("Você possui um saldo de:\n", caixaq);
+printf("O valor total recebido foi de:%f\n", quarta);
+printf("O total de gastos é igual a:%f\n", quartas);
+printf("Você possui um saldo negativo de:%f\n", caixaq);
 }
 
 //quinta-feira
@@ -76,17 +102,17 @@ scanf("%f", &quinta);
 printf("Escreva suas saídas de quinta-feira:\n");
 scanf("%f", &quintas);
 caixaqi=quinta-quintas;
-printf("Você tem um saldo de:\n", caixaqi);
+printf("Você tem um saldo de:%f\n", caixaqi);
 
 if (caixaqi > 2000){
-printf("O valor total recebido foi de:\n", quinta);
-printf("O total de gastos é igual a:\n", quintas);
-printf("Você possui um saldo de:\n", caixaqi);
+printf("O valor total recebido foi de:%f\n", quinta);
+printf("O total de gastos é igual a:%f\n", quintas);
+printf("Você possui um saldo positivo de:%f\n", caixaqi);
 }
 else if(caixaqi < -500){
-printf("O valor total recebido foi de:\n", quinta);
-printf("O total de gastos é igual a:\n", quintas);
-printf("Você possui um saldo de:\n", caixaqi);
+printf("O valor total recebido foi de:%f\n", quinta);
+printf("O total de gastos é igual a:%f\n", quintas);
+printf("Você possui um saldo negativo de:%f\n", caixaqi);
 }
 
 //sexta-feira
@@ -95,17 +121,17 @@ scanf("%f", &sexta);
 printf("Escreva suas saídas de sexta-feira:\n");
 scanf("%f", &sextas);
 caixasx=sexta-sextas;
-printf("Você tem um saldo de:\n", caixasx);
+printf("Você tem um saldo de:%f\n", caixasx);
 
 if (caixasx > 2000){
-printf("O valor total recebido foi de:\n", sexta);
-printf("O total de gastos é igual a:\n", sextas);
-printf("Você possui um saldo de:\n", caixasx);
+printf("O valor total recebido foi de:%f\n", sexta);
+printf("O total de gastos é igual a:%f\n", sextas);
+printf("Você possui um saldo positivo de:%f\n", caixasx);
 }
 else if(caixasx < -500){
-printf("O valor total recebido foi de:\n", sexta);
-printf("O total de gastos é igual a:\n", sextas);
-printf("Você possui um saldo de:\n", caixasx);
+printf("O valor total recebido foi de:%f\n", sexta);
+printf("O total de gastos é igual a:%f\n", sextas);
+printf("Você possui um saldo negativo de:%f\n", caixasx);
 }
 
 
