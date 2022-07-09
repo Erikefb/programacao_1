f11bcaa6324dbb0cce8d9f1519f7d41f3c91db8c
Erikefb <106414223+Erikefb@users.noreply.github.com>
2022-07-08 21:56:13 -0300

    Create exercicio4-2022-07-08.c

---
+++ exercicios_de_sala/exercicio4-2022-07-08.c
@@ -0,0 +1,16 @@
+/*Crie um vetor, de forma automática, com a seguinte P.A.:
+2n+1, onde n = índice do vetor.
+Ele deve ser do tipo float e ter 100 posições.
+Após isso, imprima os 10 primeiros valores do vetor na tela.*/
+
+#include <stdio.h>
+int main(){
+	float vet[100];
+	int i;
+	for(i=0; i<100; i++){
+		vet[i]=(2*i)+1;
+	}
+	for(i=0; i<10; i++){
+	printf("%.f\n", vet [i]);
+}
+}
