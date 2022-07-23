c15b64e0d4c7826cd4878301624e5186872bf3a6
Erikefb <106414223+Erikefb@users.noreply.github.com>
2022-07-22 21:22:10 -0300

    Update struct-2022-07-22.c

--- exercicios_de_sala/struct-2022-07-22.c
+++ exercicios_de_sala/struct-2022-07-22.c
@@ -12,7 +12,7 @@ int main(){
 	printf("Nome:%s\nIdade:%d\n", var1.nome, var1.idade);
 }
 
-\\OUTRA OPÇÃO
+----------------------------------------------------------------------------------------------------------------------
 #include <stdio.h>
 typedef struct{
 	char nome[20];
@@ -27,3 +27,33 @@ int main(){
 	printf("Nome:%s\nIdade:%d\n", var1.nome, var1.idade);
 }
 
+--------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+typedef struct{
+	char nome[20];
+	int idade;
+	float nota;
+}tipo_aluno;
+
+int main(){
+	tipo_aluno aluno[3];
+	int i;
+	
+	for (i=0;i<3;i++){
+	printf("Escreva o nome do aluno:\n");
+	scanf("%s", aluno[i].nome);
+	printf("Escreva a idade do aluno:\n");
+	scanf("%d", &aluno[i].idade);
+	printf("Escreva a nota do aluno:\n");
+	scanf("%f", &aluno[i].nota);
+	printf("\n");
+}
+
+	for (i=0;i<3;i++){
+	printf("Nome:%s\nIdade:%d\nNota:%.2f\n\n\n", aluno[i].nome, aluno[i].idade, aluno[i].nota);
+
+}
+
+
+
+}
